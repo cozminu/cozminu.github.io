f454ad596e3309712ee33a6d046841cca60e67cb
Cozmin <ucozmin@gmail.com>
2024-03-25 20:22:19 +0200

    march updates

--- src/components/ProfileTitle.tsx
+++ src/components/ProfileTitle.tsx
@@ -5,6 +5,7 @@ function ProfileTitle(props: any) {
     <div className="ProfileTitle">
       <h1>Cozmin Ungureanu</h1>
       <h2>Full Stack Web Developer</h2>
+      <h3>NodeJS | TypeScript | Fun stuff</h3>
     </div>
   );
 }
